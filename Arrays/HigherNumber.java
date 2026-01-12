4dcc1b87678b32046911e45940312fac036ca300
Ramit Roshann <ramitroshan777777@gmail.com>
2026-01-12 12:45:42 +0530

    solved secretsociety

--- Arrays/HigherNumber.java
+++ Arrays/HigherNumber.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 /* Write a function that returns true, if there exists at least one number that is larger than
 or equal to n.
 existsHigher([5,3,15,22,4], 10) -> true
@@ -9,7 +11,7 @@ public class HigherNumber {
     public static boolean existsHigher(int[] arr, int n){
 
         for(int i=0; i<arr.length; i++){
-            if(arr[i] > n){
+            if(arr[i] >= n){
                 return true;
             }
         }
@@ -17,12 +19,34 @@ public class HigherNumber {
     }
     public static void main(String[] args) {
         int[] array = {5,3,15,22,4};
-
-        int number = 10;
+        int number = 10; //true
         boolean ans = existsHigher(array, number);
         System.out.println(ans);
+
+        
+        //printing array (just normally printing array)
+        System.out.println(Arrays.toString(array));
     }
 }
 
 
+//2ndmethods (Flag Approach)
+// package Arrays;
+// public class HigherNumber {
+//     public static boolean existsHigher(int[] arr, int n){
+//         //flag approach
+//         boolean isHigh = false;
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i] >= n){
+//                 isHigh = true;
+//                 break; //stop when it's true
+//             }
+//         }
+//         return isHigh;
+//     }
+//     public static void main(String[] args) {
+//         System.out.println(existsHigher(new int[] {5,3,15,22,4}, 10));  //true
+//         System.out.println(existsHigher(new int[] {1,2,3,4,5}, 8)); //false
+//     }
+// }
  
---
+++ Arrays/SecretSociety.java
@@ -0,0 +1,47 @@
+package Arrays;
+
+import java.util.Arrays;
+
+/* Secret Society
+A group of friends have decided to start a secret socety. The name will be the 1st
+letter of each of their name, sorted in alphabetical order.
+create a function that takes in an array of names and returns the name of the secret society
+
+societyName(["Adam", "Sarah", "Malcolm"]) -> "AMS"
+societyName(["Harry", "Newt", "Luna", "Cho"]) -> "CHLN"
+*/
+public class SecretSociety {
+
+    public static String societyName(String[] names){
+
+        //We use char because we are storing single letters (in output)
+        char[] letter = new char[names.length];
+
+        for(int i=0; i<names.length; i++){
+            letter[i] = Character.toUpperCase(names[i].charAt(0));
+        }
+        //sort letters
+        Arrays.sort(letter);
+
+        //convert char array to string 
+        return new String(letter);
+    }
+    public static void main(String[] args) {
+        String[] names1 = {"Adam", "Sarah", "Malcolm"};
+        System.out.println(societyName(names1)); // AMS
+
+        String[] names2 = {"Harry", "Newt", "Luna", "Cho"};
+        System.out.println(societyName(names2)); // CHLN
+    }
+}
+
+
+ 
+
+/* 
+
+
+A, S, M are characters, not words.
+In Java, a single letter = char.
+“We use char because we are storing single letters, and sorting characters is easier and efficient.”
+*/
