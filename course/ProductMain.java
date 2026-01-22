d6f2dce0289b89d7941c6a7330c113f9589d734d
Ramit Roshann <ramitroshan777777@gmail.com>
2026-01-23 00:29:13 +0530

    Implement Product, Book, and Accessories classes using inheritance and method overriding

--- course/ProductMain.java
+++ course/ProductMain.java
@@ -2,7 +2,32 @@ package course;
 
 public class ProductMain {
     public static void main(String[] args) {
-        
+        //Book Object
+        Book b = new Book();
+        b.name = "Let's end pseudo Secularism";
+        b.price = 300;
+        b.bId = 101;
+        b.author = "Ramit Roshan";
+        b.genre = "Future OF Bharat";
+        b.pages  = 300;
+
+        b.applyDiscount(10);
+        System.out.println(b);
+         System.out.println("Reading time: " + b.getReadingTime() + " hours");
+
+
+         // Accessories object
+        Accessories acc = new Accessories();
+        acc.name = "Headphones";
+        acc.price = 2000;
+        acc.accessoryId = 201;
+        acc.type = "Audio";
+        acc.brand = "JBL";
+        acc.isWarranty = true;
+
+        acc.applyDiscount(15);
+        System.out.println(acc);
+        System.out.println("Warranty Available: " + acc.isWarrantyAvailable());
     }
 }
 
@@ -13,7 +38,8 @@ class Product{
     // Instance method to apply discount
     public void applyDiscount(double percentage) {
         if (percentage > 0 && percentage <= 100) {
-            price = price - (int) (price * percentage / 100); // calculating discount
+            // price = price - (int) (price * percentage / 100); // calculating discount
+            price -= price * percentage / 100;
         } else {
             System.out.println("Invalid discount percentage");
         }
@@ -28,5 +54,39 @@ class Book extends Product{
     int bId;
     String author;
     String genre;
-    int page;
+    int pages;
+
+    // Child-specific instance method
+    public int getReadingTime(){
+        // Assume 20 pages per hour
+        return pages / 20;
+    }
+
+    @Override //label
+    public String toString(){
+        return "Book[bId=" + bId + ",author=" + author + ",genre=" + genre + ",pages=" + pages + ",price=" + price+ "]";
+    }
+}
+
+//Create a class Accessories that extends Product.
+class Accessories extends Product{
+    int accessoryId;
+    String type;
+    String brand;
+    Boolean isWarranty;
+
+    // Child-specific instance method
+    public boolean isWarrantyAvailable() {
+        return isWarranty;
+    }
+
+    @Override
+    public String toString() {
+        return "Accessories [id=" + accessoryId +
+                ", name=" + name +
+                ", type=" + type +
+                ", brand=" + brand +
+                ", price=" + price +
+                ", warranty=" + isWarranty + "]";
+    }
 }
